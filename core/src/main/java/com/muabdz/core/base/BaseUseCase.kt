11f88031086236383d8e7cd344c1bd8dbde74459
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2023-07-04 13:11:17 +0700

    add base usecase class

---
+++ core/src/main/java/com/muabdz/core/base/BaseUseCase.kt
@@ -0,0 +1,19 @@
+package com.muabdz.core.base
+
+import com.muabdz.core.wrapper.ViewResource
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import java.lang.RuntimeException
+
+abstract class BaseUseCase<P, R: Any?> constructor(private val dispatcher: CoroutineDispatcher) {
+    suspend operator fun invoke(param: P? = null): Flow<ViewResource<R>> {
+        return execute(param)
+            .catch { emit(ViewResource.Error(Exception(it))) }
+            .flowOn(dispatcher)
+    }
+
+    @Throws(RuntimeException::class)
+    abstract suspend fun execute(param: P? = null): Flow<ViewResource<R>>
+}
