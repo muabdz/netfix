27bb70239125266623eaa21afb3f48ab8b3d228b
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2023-07-13 21:42:52 +0700

    fix shared api repository dependency

--- shared/src/main/java/com/muabdz/shared/data/repository/SharedApiRepository.kt
+++ shared/src/main/java/com/muabdz/shared/data/repository/SharedApiRepository.kt
@@ -2,7 +2,7 @@ package com.muabdz.shared.data.repository
 
 import com.muabdz.core.wrapper.DataResource
 import com.muabdz.shared.data.model.request.WatchlistRequest
-import com.muabdz.shared.data.remote.services.SharedFeatureApi
+import com.muabdz.shared.data.remote.datasource.SharedFeatureApiDataSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
@@ -14,17 +14,17 @@ interface SharedApiRepository {
 }
 
 class SharedApiRepositoryImpl(
-    private val service: SharedFeatureApi
+    private val dataSource: SharedFeatureApiDataSource
 ) : Repository(), SharedApiRepository {
     override suspend fun addWatchlist(movieId: String): Flow<DataResource<Any>> {
         return flow {
-            emit(safeNetworkCall { service.addWatchlist(WatchlistRequest(movieId)) })
+            emit(safeNetworkCall { dataSource.addWatchlist(WatchlistRequest(movieId)) })
         }
     }
 
     override suspend fun removeWatchlist(movieId: String): Flow<DataResource<Any>> {
         return flow {
-            emit(safeNetworkCall { service.removeWatchlist(WatchlistRequest(movieId)) })
+            emit(safeNetworkCall { dataSource.removeWatchlist(WatchlistRequest(movieId)) })
         }
     }
 
