f49d3c7b083f03c0ae3563c844e3777d826594dc
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2023-07-13 21:23:18 +0700

    implement get user token use case in network client

--- shared/src/main/java/com/muabdz/shared/data/remote/NetworkClient.kt
+++ shared/src/main/java/com/muabdz/shared/data/remote/NetworkClient.kt
@@ -2,18 +2,36 @@ package com.muabdz.shared.data.remote
 
 import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.muabdz.core.BuildConfig
+import com.muabdz.shared.domain.GetUserTokenUseCase
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-// TODO: prepare use case to get token
-class NetworkClient(val chuckerInterceptor: ChuckerInterceptor) {
+class NetworkClient(
+    val getUserTokenUseCase: GetUserTokenUseCase,
+    val chuckerInterceptor: ChuckerInterceptor) {
 
     inline fun <reified I> create(): I {
+        val authInterceptor = Interceptor{
+            val requestBuilder = it.request().newBuilder()
+            runBlocking {
+                getUserTokenUseCase().first { tokenResponse ->
+                    val token = tokenResponse.payload
+                    if (!token.isNullOrEmpty()) {
+                        requestBuilder.addHeader("Authorization", "Bearer $token")
+                    }
+                    true
+                }
+            }
+            it.proceed(requestBuilder.build())
+        }
 
         val okHttpClient = OkHttpClient.Builder()
-            // TODO: add auth interceptor
+            .addInterceptor(authInterceptor)
             .addInterceptor(chuckerInterceptor)
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
