8468a6d0d6d884c171ca20360e1db6525281ca86
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2023-07-13 20:48:33 +0700

    add repository classes

---
+++ shared/src/main/java/com/muabdz/shared/repository/SharedApiRepository.kt
@@ -0,0 +1,31 @@
+package com.muabdz.shared.repository
+
+import com.muabdz.core.wrapper.DataResource
+import com.muabdz.shared.data.model.request.WatchlistRequest
+import com.muabdz.shared.data.remote.services.SharedFeatureApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+interface SharedApiRepository {
+    suspend fun addWatchlist(movieId: String): Flow<DataResource<Any>>
+
+    suspend fun removeWatchlist(movieId: String): Flow<DataResource<Any>>
+
+}
+
+class SharedApiRepositoryImpl(
+    private val service: SharedFeatureApi
+) : Repository(), SharedApiRepository {
+    override suspend fun addWatchlist(movieId: String): Flow<DataResource<Any>> {
+        return flow {
+            emit(safeNetworkCall { service.addWatchlist(WatchlistRequest(movieId)) })
+        }
+    }
+
+    override suspend fun removeWatchlist(movieId: String): Flow<DataResource<Any>> {
+        return flow {
+            emit(safeNetworkCall { service.removeWatchlist(WatchlistRequest(movieId)) })
+        }
+    }
+
+}
