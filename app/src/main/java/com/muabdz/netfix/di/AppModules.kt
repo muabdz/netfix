ac729dfb60b12ccef24afcde77497353286b6be9
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2023-07-17 06:16:29 +0700

    add app module

---
+++ app/src/main/java/com/muabdz/netfix/di/AppModules.kt
@@ -0,0 +1,10 @@
+package com.muabdz.netfix.di
+
+import com.muabdz.core.base.BaseModule
+import org.koin.core.module.Module
+
+object AppModules : BaseModule {
+    // TODO: add router modules
+    override fun getModules(): List<Module> = listOf()
+
+}
