3b0ab4f16ad4a51e708f15496863894009cc4af4
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2023-07-17 06:17:02 +0700

    add app class and internet permission

---
+++ app/src/main/java/com/muabdz/netfix/App.kt
@@ -0,0 +1,25 @@
+package com.muabdz.netfix
+
+import android.app.Application
+import com.muabdz.netfix.di.AppModules
+import com.muabdz.shared.di.SharedModules
+import com.muabdz.splashscreen.di.SplashScreenModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(
+                AppModules.getModules() +
+                SharedModules.getModules() +
+                SplashScreenModules.getModules()
+            )
+        }
+    }
+}
