fba65c628405ea48d0880548dd51671f799e6995
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2023-07-17 05:50:36 +0700

    add splash screen activity

---
+++ feature/splashscreen/src/main/java/com/muabdz/splashscreen/presentation/SplashScreenActivity.kt
@@ -0,0 +1,42 @@
+package com.muabdz.splashscreen.presentation
+
+import android.widget.Toast
+import com.muabdz.core.base.BaseActivity
+import com.muabdz.shared.utils.ext.subscribe
+import com.muabdz.splashscreen.databinding.ActivitySplashScreenBinding
+import org.koin.android.ext.android.inject
+
+class SplashScreenActivity : BaseActivity<ActivitySplashScreenBinding, SplashScreenViewModel>(ActivitySplashScreenBinding::inflate) {
+    override val viewModel: SplashScreenViewModel by inject()
+
+    override fun initView() {
+        viewModel.syncUser()
+    }
+
+    override fun observeData() {
+        viewModel.syncResult.observe(this) {
+            it.subscribe(
+                doOnSuccess = { response ->
+                    if (response.payload?.first == true) {
+                        navigateToHome()
+                    } else {
+                        navigateToLogin()
+                    }
+                },
+                doOnError = { error ->
+                    Toast.makeText(this, error.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            )
+        }
+    }
+
+    private fun navigateToLogin() {
+        Toast.makeText(this, "Navigate to login", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun navigateToHome() {
+        Toast.makeText(this, "Navigate to home", Toast.LENGTH_SHORT).show()
+    }
+
+
+}
