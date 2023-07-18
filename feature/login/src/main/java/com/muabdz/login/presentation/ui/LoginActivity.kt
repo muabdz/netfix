0f039fdb6f84ff8dca9cb90984445e16cf921d8a
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2023-07-18 14:38:40 +0700

    add home and login activity

---
+++ feature/login/src/main/java/com/muabdz/login/presentation/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.muabdz.login.presentation.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.muabdz.login.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
