b580d37dc15dc22b81ac011df04d32be3c856569
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2023-07-18 14:43:27 +0700

    add routers in app module for injection

--- app/src/main/java/com/muabdz/netfix/di/AppModules.kt
+++ app/src/main/java/com/muabdz/netfix/di/AppModules.kt
@@ -1,10 +1,15 @@
 package com.muabdz.netfix.di
 
 import com.muabdz.core.base.BaseModule
+import com.muabdz.netfix.router.ActivityRouterImpl
+import com.muabdz.shared.router.ActivityRouter
 import org.koin.core.module.Module
+import org.koin.dsl.module
 
 object AppModules : BaseModule {
-    // TODO: add router modules
     override fun getModules(): List<Module> = listOf()
 
+    val routers: Module = module {
+        single<ActivityRouter> { ActivityRouterImpl() }
+    }
 }
