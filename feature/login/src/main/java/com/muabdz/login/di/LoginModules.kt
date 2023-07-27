3a7c3ff2a0d3000be647f924ca4ea6a095f5dc9b
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2023-07-27 15:51:58 +0700

    update login modules

--- feature/login/src/main/java/com/muabdz/login/di/LoginModules.kt
+++ feature/login/src/main/java/com/muabdz/login/di/LoginModules.kt
@@ -6,7 +6,12 @@ import com.muabdz.login.data.network.datasource.LoginDataSourceImpl
 import com.muabdz.login.data.repository.LoginRepository
 import com.muabdz.login.data.repository.LoginRepositoryImpl
 import com.muabdz.login.data.service.LoginFeatureApi
+import com.muabdz.login.domain.CheckLoginFieldUseCase
+import com.muabdz.login.domain.LoginUserUseCase
+import com.muabdz.login.presentation.ui.LoginViewModel
 import com.muabdz.shared.data.remote.NetworkClient
+import kotlinx.coroutines.Dispatchers
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -15,13 +20,14 @@ object LoginModules : FeatureModules {
         single<LoginRepository> { LoginRepositoryImpl(get()) }
     }
     override val viewModels: Module = module {
-        // TODO: implement later
+        viewModelOf(::LoginViewModel)
     }
     override val dataSources: Module = module {
         single<LoginDataSource> { LoginDataSourceImpl(get()) }
     }
     override val useCases: Module = module {
-        // TODO: implement later
+        single { CheckLoginFieldUseCase(Dispatchers.IO) }
+        single { LoginUserUseCase(get(), get(), get(), Dispatchers.IO) }
     }
     override val network: Module = module {
         single<LoginFeatureApi> { get<NetworkClient>().create() }
