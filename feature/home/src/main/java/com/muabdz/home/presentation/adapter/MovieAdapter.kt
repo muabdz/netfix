62ba2cd4d2c8a2da56d6b7f200088e3a29a360a4
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2023-08-07 21:53:00 +0700

    add home section view holder and movie adapter

---
+++ feature/home/src/main/java/com/muabdz/home/presentation/adapter/MovieAdapter.kt
@@ -0,0 +1,76 @@
+package com.muabdz.home.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.muabdz.shared.data.model.viewparam.MovieViewParam
+import com.muabdz.styling.databinding.ItemMoviePosterBinding
+import com.muabdz.styling.databinding.ItemMoviePosterGridBinding
+
+class MovieAdapter(
+    private val isGridLayout: Boolean = false,
+    private val itemClicked: (MovieViewParam) -> Unit
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val items = mutableListOf<MovieViewParam>()
+
+    fun setItems(items: List<MovieViewParam>) {
+        this.items.clear()
+        this.items.addAll(items)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (!isGridLayout) {
+            PosterViewHolderImpl(
+                ItemMoviePosterBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+                itemClicked
+            )
+        } else {
+            GridPosterViewHolderImpl(
+                ItemMoviePosterGridBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                ),
+                itemClicked
+            )
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as PosterViewHolder).bindView(items[position])
+    }
+
+    override fun getItemCount(): Int = items.size
+
+}
+
+interface PosterViewHolder {
+    fun bindView(item: MovieViewParam)
+}
+
+class PosterViewHolderImpl(
+    private val binding: ItemMoviePosterBinding,
+    private val itemClicked: (MovieViewParam) -> Unit
+) : RecyclerView.ViewHolder(binding.root), PosterViewHolder {
+    override fun bindView(item: MovieViewParam) {
+        with(item) {
+            binding.ivPoster.load(item.posterUrl)
+            itemView.setOnClickListener { itemClicked.invoke(this) }
+        }
+    }
+}
+
+class GridPosterViewHolderImpl(
+    private val binding: ItemMoviePosterGridBinding,
+    private val itemClicked: (MovieViewParam) -> Unit
+) : RecyclerView.ViewHolder(binding.root), PosterViewHolder {
+    override fun bindView(item: MovieViewParam) {
+        with(item) {
+            binding.ivPoster.load(item.posterUrl)
+            itemView.setOnClickListener { itemClicked.invoke(this) }
+        }
+    }
+}
