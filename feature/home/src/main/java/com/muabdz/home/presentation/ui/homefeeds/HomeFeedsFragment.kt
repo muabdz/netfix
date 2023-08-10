5ce7ca76cae2c36928277c40134afca184a7944f
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2023-08-10 21:26:03 +0700

    update home feeds fragment class

--- feature/home/src/main/java/com/muabdz/home/presentation/ui/homefeeds/HomeFeedsFragment.kt
+++ feature/home/src/main/java/com/muabdz/home/presentation/ui/homefeeds/HomeFeedsFragment.kt
@@ -1,20 +1,88 @@
 package com.muabdz.home.presentation.ui.homefeeds
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.muabdz.home.R
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.muabdz.core.base.BaseFragment
+import com.muabdz.home.databinding.FragmentHomeFeedsBinding
+import com.muabdz.home.presentation.adapter.viewholder.HomeAdapter
+import com.muabdz.home.presentation.adapter.viewholder.HomeAdapterClickListener
+import com.muabdz.home.presentation.ui.home.HomeViewModel
+import com.muabdz.shared.data.model.viewparam.MovieViewParam
+import com.muabdz.shared.utils.ext.subscribe
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 
-class HomeFeedsFragment : Fragment() {
+class HomeFeedsFragment : BaseFragment<FragmentHomeFeedsBinding, HomeViewModel>(
+    FragmentHomeFeedsBinding::inflate
+) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_feeds, container, false)
+    override val viewModel: HomeViewModel by sharedViewModel()
+
+    private val recyclerViewPool: RecyclerView.RecycledViewPool by lazy {
+        RecyclerView.RecycledViewPool()
     }
+
+    private val homeAdapter: HomeAdapter by lazy {
+        HomeAdapter(object : HomeAdapterClickListener {
+            override fun onMyListClicked(movieViewParam: MovieViewParam) {
+                // TODO: add to watchlist
+            }
+
+            override fun onPlayMovieClicked(movieViewParam: MovieViewParam) {
+                // TODO: go to player
+            }
+
+            override fun onMovieClicked(movieViewParam: MovieViewParam) {
+                // TODO: show movie info
+            }
+        }, recyclerViewPool)
+    }
+
+    private fun setupRecyclerView() {
+        binding.rvHome.apply {
+            adapter = homeAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            setRecycledViewPool(recycledViewPool)
+            // TODO: add scroll listener
+        }
+    }
+
+
+    override fun initView() {
+        setupRecyclerView()
+        initData()
+    }
+
+    override fun observeData() {
+        super.observeData()
+        viewModel.homeFeedsResult.observe(this) {
+            it.subscribe(
+                doOnSuccess = { result ->
+                    showLoading(false)
+                    result.payload?.let { data ->
+                        homeAdapter.setItems(data)
+                    }
+                },
+                doOnLoading = {
+                    showLoading(true)
+                },
+                doOnError = { error ->
+                    showLoading(false)
+                    error.exception?.let { e -> showError(true, e) }
+
+                })
+        }
+        // TODO: add observe data
+    }
+
+    private fun initData() {
+        viewModel.getCurrentUser()
+        viewModel.fetchHome()
+    }
+
+    private fun showLoading(isShowLoading: Boolean) {
+        binding.pbHome.isVisible = isShowLoading
+    }
+
 }
