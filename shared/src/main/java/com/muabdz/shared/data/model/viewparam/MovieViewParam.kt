28eaf14facc20544e3d07603993f812e2b69fcf7
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2023-08-10 21:53:26 +0700

    add watchlist button use case

--- shared/src/main/java/com/muabdz/shared/data/model/viewparam/MovieViewParam.kt
+++ shared/src/main/java/com/muabdz/shared/data/model/viewparam/MovieViewParam.kt
@@ -1,14 +1,12 @@
 package com.muabdz.shared.data.model.viewparam
 
-import com.google.gson.annotations.SerializedName
-
 class MovieViewParam (
     val cast: List<String>,
     val category: List<String>,
     val director: String,
     val filmRate: String,
     val id: Int,
-    val isUserWatchlist: Boolean,
+    var isUserWatchlist: Boolean,
     val overview: String,
     val posterUrl: String,
     val releaseDate: String,
