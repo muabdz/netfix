c611a80a3dd83097526e921623ec46e41286b5bc
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2023-09-25 22:09:08 +0700

    add profile activity

---
+++ feature/profile/src/main/java/com/muabdz/profile/presentation/ui/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.muabdz.profile.presentation.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.muabdz.profile.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
