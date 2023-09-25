7e543fe912d3136f2bf339d0e7a84d331326a4c2
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2023-09-25 14:33:17 +0700

    add profile module class to app module

--- app/src/main/java/com/muabdz/netfix/App.kt
+++ app/src/main/java/com/muabdz/netfix/App.kt
@@ -5,6 +5,7 @@ import com.muabdz.detail.di.DetailModules
 import com.muabdz.home.di.HomeModules
 import com.muabdz.login.di.LoginModules
 import com.muabdz.netfix.di.AppModules
+import com.muabdz.profile.di.ProfileModules
 import com.muabdz.register.di.RegisterModules
 import com.muabdz.shared.di.SharedModules
 import com.muabdz.splashscreen.di.SplashScreenModules
@@ -26,7 +27,8 @@ class App: Application() {
                 LoginModules.getModules() +
                 RegisterModules.getModules() +
                 HomeModules.getModules() +
-                DetailModules.getModules()
+                DetailModules.getModules() +
+                ProfileModules.getModules()
             )
         }
     }
